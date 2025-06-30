239a510176f1dad1e087ac34578440841685ca35
Naclara08 <ana.maciel.matias@escola.pr.gov.br>
2025-06-30 10:40:34 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+   'Historia',
+   'Quem foi o primeiro Presidente do Brasil?',
+    'O primeiro presidente foi Marechal Deodoro da Fonseca?'
+)
+
+criaCartao(
+    'Matematica',
+    'Quanto é 2+2',
+    '2+2 é igual a 4'
+)
